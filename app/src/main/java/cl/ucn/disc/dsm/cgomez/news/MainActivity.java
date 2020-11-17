61f763a13d78a8543cd744d9d5e2ee0c65d16a6b
g-car <carlosomero@gmail.com>
2020-11-16 23:56:47 -0300

    listo

--- app/src/main/java/cl/ucn/disc/dsm/cgomez/news/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dsm/cgomez/news/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//ff
+//ffff
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
